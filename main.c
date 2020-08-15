3af16289ad19e622b722c15d014826c2f9930401
yahya-mohammed07 <goldroger300@gmail.com>
2020-08-15 10:18:35 +0300

    on progress

--- main.c
+++ main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     printf("-Enter a credit number: ");
     long int number = 4003600000000014;
-   //scanf("%d", &credit);
+    char* theNumber;
+    sprintf(theNumber, "%ld", number); 
     int sum = 0;
-	//*
-    while (number != 0)
+    int size = strlen(theNumber);
+    //*
+    for (int i = size - 1; i > 0; i -= 2)
     {
-        sum += number % 10;
-        number /= 10;
+        sum += theNumber[i] - '0';
     }
+    for(int i = size -2; i >= 0; i -= 2)
+	{
+		int temp = 0;
+		temp += (theNumber[i]-'0') * 2;
+		if(temp > 9)
+		{
+			temp -= 9;
+		}
+		sum += temp;
+	}
     
+    printf("%d",sum);
 	//*    
 }
